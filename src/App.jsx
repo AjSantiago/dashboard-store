a5bbc43fdb697c1eefbc87fc690c20ecd4075be8
AjSantiago <isc_aj.santiago@hotmail.com>
2023-03-21 00:43:41 -0600

    Main App done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import { useState } from 'react'
-import {
-  RiAddLine,
-  RiArrowDownSLine,
-  RiCloseLine,
-  RiMenu3Fill,
-  RiPieChartLine,
-  RiSearchLine,
-  RiUser3Line
-} from 'react-icons/ri'
-
+import { RiArrowDownSLine } from 'react-icons/ri'
+import { Card } from './components/shared/Card'
+import { Cart } from './components/shared/Cart'
+import { Header } from './components/shared/Header'
+import { MenuMovil } from './components/shared/MenuMovil'
 import { Sidebar } from './components/shared/Sidebar'
 
 function App() {
@@ -17,69 +12,29 @@ function App() {
 
   const toggleMenu = () => {
     setShowMenu(!showMenu)
+    setShowOrder(false)
+  }
+  const toggleOrder = () => {
+    setShowOrder(!showOrder)
+    setShowMenu(false)
   }
 
   return (
     <div className='bg-[#262837] w-full min-h-screen'>
       <Sidebar showMenu={showMenu} />
-      {/* Menu móvil */}
-      <nav className='bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 px-8 py-2 flex items-center justify-between rounded-tl-xl rounded-tr-xl'>
-        <button className='p-2'>
-          <RiUser3Line />
-        </button>
-        <button className='p-2'>
-          <RiAddLine />
-        </button>
-        <button className='p-2'>
-          <RiPieChartLine />
-        </button>
-        <button onClick={toggleMenu} className='text-white p-2'>
-          {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
-        </button>
-      </nav>
 
-      <main className='lg:pl-28 grid grid-cols-1 lg:grid-cols-8 p-4 pb-20'>
-        <div className='lg:col-span-6 md:p-4'>
+      {/* Menu móvil */}
+      <MenuMovil
+        toggleOrder={toggleOrder}
+        toggleMenu={toggleMenu}
+        showMenu={showMenu}
+      />
+
+      <main className='lg:pl-28 pb-10 lg:pr-96'>
+        <div className='md:p-4 p-4'>
           {/* Header */}
-          <header>
-            {/* Title and search */}
-            <div className='flex flex-col gap-4 mb-6 md:flex-row md:justify-between md:items-center'>
-              <div>
-                <h1 className='text-2xl text-gray-300'>Resto</h1>
-                <p className='text-gray-500'>
-                  {new Date().toLocaleDateString()}
-                </p>
-              </div>
-              <form>
-                <div className='w-full relative'>
-                  <RiSearchLine className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-300' />
-                  <input
-                    type='text'
-                    placeholder='Search'
-                    className='bg-[#1F1D2B] w-full py-2 pl-10 pr-4 rounded-lg text-gray-300 outline-none'
-                  />
-                </div>
-              </form>
-            </div>
-            {/* Tabs */}
-            <nav className='text-gray-300 flex items-center justify-between border-b mb-6 md:justify-start md:gap-8'>
-              <a
-                href='#'
-                className='relative py-2 pr-4 before:w-1/2 before:h-[2px] before:absolute before:bg-[#ec7c6a] before:left-0 before:rounded-full before:-bottom-[1px] text-[#ec7c6a]'
-              >
-                Hot dishes
-              </a>
-              <a href='#' className=' py-2 pr-4'>
-                Cold dishes
-              </a>
-              <a href='#' className=' py-2 pr-4'>
-                Soup
-              </a>
-              <a href='#' className=' py-2'>
-                Grill
-              </a>
-            </nav>
-          </header>
+          <Header />
+
           {/* Title content */}
           <div className='flex items-center justify-between mb-16'>
             <h2 className='text-xl text-gray-300'>Choose Dishes</h2>
@@ -90,68 +45,52 @@ function App() {
           {/* Content */}
           <div className='p-2 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
             {/* Card */}
-            <div className='bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-gray-300 text-center'>
-              <img
-                src='../public/comida_01.png'
-                className='w-28 h-28 object-cover -mt-16  shadow-2xl rounded-full'
-              />
-              <p className='text-[18px]'>Speacy seasoned seafood nodles</p>
-              <span className='text-gray-400'>$ 2.29</span>
-              <p className='text-gray-600'>20 Bowls available</p>
-            </div>
+            <Card
+              img='../public/comida_01.png'
+              description='Speacy seasoned seafood nodles'
+              price='2.29'
+              inventory='20'
+            />
             {/* Card */}
-            <div className='bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-gray-300 text-center'>
-              <img
-                src='../public/comida_01.png'
-                className='w-28 h-28 object-cover -mt-16  shadow-2xl rounded-full'
-              />
-              <p className='text-[18px]'>Speacy seasoned seafood nodles</p>
-              <span className='text-gray-400'>$ 2.29</span>
-              <p className='text-gray-600'>20 Bowls available</p>
-            </div>
+            <Card
+              img='../public/comida_02.png'
+              description='Speacy seasoned seafood nodles'
+              price='2.29'
+              inventory='20'
+            />
             {/* Card */}
-            <div className='bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-gray-300 text-center'>
-              <img
-                src='../public/comida_01.png'
-                className='w-28 h-28 object-cover -mt-16  shadow-2xl rounded-full'
-              />
-              <p className='text-[18px]'>Speacy seasoned seafood nodles</p>
-              <span className='text-gray-400'>$ 2.29</span>
-              <p className='text-gray-600'>20 Bowls available</p>
-            </div>
+            <Card
+              img='../public/comida_01.png'
+              description='Speacy seasoned seafood nodles'
+              price='2.29'
+              inventory='20'
+            />
             {/* Card */}
-            <div className='bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-gray-300 text-center'>
-              <img
-                src='../public/comida_01.png'
-                className='w-28 h-28 object-cover -mt-16  shadow-2xl rounded-full'
-              />
-              <p className='text-[18px]'>Speacy seasoned seafood nodles</p>
-              <span className='text-gray-400'>$ 2.29</span>
-              <p className='text-gray-600'>20 Bowls available</p>
-            </div>
+            <Card
+              img='../public/comida_02.png'
+              description='Speacy seasoned seafood nodles'
+              price='2.29'
+              inventory='20'
+            />
             {/* Card */}
-            <div className='bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-gray-300 text-center'>
-              <img
-                src='../public/comida_01.png'
-                className='w-28 h-28 object-cover -mt-16  shadow-2xl rounded-full'
-              />
-              <p className='text-[18px]'>Speacy seasoned seafood nodles</p>
-              <span className='text-gray-400'>$ 2.29</span>
-              <p className='text-gray-600'>20 Bowls available</p>
-            </div>
+            <Card
+              img='../public/comida_01.png'
+              description='Speacy seasoned seafood nodles'
+              price='2.29'
+              inventory='20'
+            />
             {/* Card */}
-            <div className='bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center gap-2 text-gray-300 text-center'>
-              <img
-                src='../public/comida_01.png'
-                className='w-28 h-28 object-cover -mt-16  shadow-2xl rounded-full'
-              />
-              <p className='text-[18px]'>Speacy seasoned seafood nodles</p>
-              <span className='text-gray-400'>$ 2.29</span>
-              <p className='text-gray-600'>20 Bowls available</p>
-            </div>
+            <Card
+              img='../public/comida_02.png'
+              description='Speacy seasoned seafood nodles'
+              price='2.29'
+              inventory='20'
+            />
           </div>
         </div>
-        <div className='lg:col-span-2 fixed lg:static right-0'>Carrito</div>
+
+        {/* Cart */}
+        <Cart showOrder={showOrder} setShowOrder={setShowOrder} />
       </main>
     </div>
   )
